7f699f996a6d5e90ad0795f3f990277cd996eeaa
Erlend Vaboen <erlendvaboen@dhcp-10-22-2-36.wlan.ntnu.no>
2022-03-22 14:33:57 +0100

    project setup

---
+++ android/src/no/ntnu/viruswar/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package no.ntnu.viruswar;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import no.ntnu.viruswar.VirusWar;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new VirusWar(), config);
+	}
+}

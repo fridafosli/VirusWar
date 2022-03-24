619255d434cd724a31dac1865c263fc81d404432
Erlend Vaboen <erlendvaboen@Erlends-MacBook-Air.local>
2022-03-24 19:20:03 +0100

    Add settings

--- core/src/no/ntnu/viruswar/MainMenu.java
+++ core/src/no/ntnu/viruswar/MainMenu.java
@@ -22,84 +22,78 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class MainMenu extends State {
 
-        protected Stage stage;
-        protected Skin skin;
-        protected Table table;
-        protected Sprite background;
-        private  TextButton playBtn;
-        private  TextButton tutorialBtn;
-        private  TextButton settingsBtn;
+    protected Stage stage;
+    protected Skin skin;
+    protected Table table;
+    protected Sprite background;
+    private TextButton playBtn;
+    private TextButton tutorialBtn;
+    private TextButton settingsBtn;
 
 
-        public MainMenu(GameStateManager gsm) {
-            super(gsm);
-            skin = new Skin(Gdx.files.internal("uiskin.json"));
-            stage = new Stage(new ScreenViewport());
+    public MainMenu(final GameStateManager gsm) {
+        super(gsm);
+        skin = new Skin(Gdx.files.internal("uiskin.json"));
+        stage = new Stage(new ScreenViewport());
 //
-            table = new Table();
-            table.setWidth(stage.getWidth());
-            table.align(Align.center|Align.top);
+        table = new Table();
+        table.setWidth(stage.getWidth());
+        table.align(Align.center | Align.top);
 
-            table.setPosition(0, Gdx.graphics.getHeight());
-
-
-
-            //InputMultiplexer im = new InputMultiplexer(stage,this);
+        table.setPosition(0, Gdx.graphics.getHeight());
 
 
-            playBtn = new TextButton("Play", skin);
-            tutorialBtn = new TextButton("Tutorial", skin);
-            settingsBtn = new TextButton("Settings", skin);
+        //InputMultiplexer im = new InputMultiplexer(stage,this);
 
-            settingsBtn.addListener(new ClickListener() {
-                @Override
-                public void clicked(InputEvent event, float x, float y) {
-                    Gdx.app.log("Settings", "clicked");
-                   // gsm.push(new SettingsMenu(gsm));
-                }
-            });
+
+        playBtn = new TextButton("Play", skin);
+        tutorialBtn = new TextButton("Tutorial", skin);
+        settingsBtn = new TextButton("Settings", skin);
+
+        settingsBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.log("Settings", "clicked");
+                gsm.push(new Settings(gsm));
+            }
+        });
 //
-            table.padTop(150);
-            table.add(playBtn).padBottom(30);
-            table.row();
-            table.add(tutorialBtn).padBottom(30);
-            table.row();
-            table.add(settingsBtn);
+        table.padTop(150);
+        table.add(playBtn).padBottom(30);
+        table.row();
+        table.add(tutorialBtn).padBottom(30);
+        table.row();
+        table.add(settingsBtn);
 
-            stage.addActor(table);
-            Gdx.input.setInputProcessor(stage);
+        stage.addActor(table);
+        Gdx.input.setInputProcessor(stage);
 
-            background = new Sprite(new Texture(Gdx.files.internal("badlogic.jpg")));
-            background.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        }
+        background = new Sprite(new Texture(Gdx.files.internal("badlogic.jpg")));
+        background.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+    }
 
-        @Override
-        protected void handleInput() {
-
-        }
-
-        @Override
-        public void update(float dt) {
-
-        }
-
-        public void render(SpriteBatch sb) {
-            // System.out.println("main "+ gsm.peek());
-
-//            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-//            sb.begin();
-//            background.draw(sb);
-//            sb.end();
-
-            stage.act(Gdx.graphics.getDeltaTime());
-            stage.draw();
-
-
-        }
-
-        @Override
-        public void dispose() {
-
-        }
+    @Override
+    protected void handleInput() {
 
     }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    public void render(SpriteBatch sb) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
+
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+}
--- core/src/no/ntnu/viruswar/Settings.java
+++ core/src/no/ntnu/viruswar/Settings.java
@@ -1,5 +1,84 @@
 package no.ntnu.viruswar;
 
-public class Settings {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class Settings extends State {
+
+    protected Stage stage;
+    protected Skin skin;
+    protected Table table;
+    protected Sprite background;
+
+    private TextButton backBtn;
+
+
+    public Settings(final GameStateManager gsm) {
+        super(gsm);
+        skin = new Skin(Gdx.files.internal("uiskin.json"));
+        stage = new Stage(new ScreenViewport());
+
+        table = new Table();
+        table.setWidth(stage.getWidth());
+        table.align(Align.center | Align.top);
+
+        table.setPosition(0, Gdx.graphics.getHeight());
+
+        backBtn = new TextButton("Back", skin);
+        backBtn.setPosition(0,Gdx.graphics.getHeight()-50);
+
+        backBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.log("Settings", "clicked");
+                gsm.set(new MainMenu(gsm));
+            }
+        });
+        stage.addActor(backBtn);
+        stage.addActor(table);
+        Gdx.input.setInputProcessor(stage);
+
+        background = new Sprite(new Texture(Gdx.files.internal("settings.png")));
+        background.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    public void render(SpriteBatch sb) {
+
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        sb.begin();
+        background.draw(sb);
+        sb.end();
+
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
+
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 
 }

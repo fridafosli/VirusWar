ceff209495a8e497308641011438e3c32b4cd631
Therese <therese.sigmundstad@gmail.com>
2022-03-28 14:19:15 +0200

    Add files

---
+++ core/src/no/ntnu/viruswar/FireBaseInterface.java
@@ -0,0 +1,16 @@
+package no.ntnu.viruswar;
+public interface FireBaseInterface {
+
+
+    public void SomeFunction();
+
+
+    public void FirstFireBaseTest();
+/*
+    public void SetOnValueChangedListener(DataHolderClass dataholder);
+
+    public void SetValueInDb(String target, String value);
+
+     */
+
+}
--- core/src/no/ntnu/viruswar/VirusWar.java
+++ core/src/no/ntnu/viruswar/VirusWar.java
@@ -4,31 +4,51 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
-
 public class VirusWar extends ApplicationAdapter {
 	SpriteBatch batch;
-	private GameStateManager gsm;
-	
+	GameStateManager gsm;
+	FireBaseInterface _FBIC;
+//	private Stage stage;
+//	private Skin skin;
+
+	public VirusWar(FireBaseInterface FBIC){
+		_FBIC=FBIC;
+
+	}
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		gsm = new GameStateManager();
 		gsm.push(new MainMenu(gsm));
+
+		gsm.render(batch);
+		_FBIC.SomeFunction();
+		_FBIC.FirstFireBaseTest();
 	}
+
+//		img = new Texture("badlogic.jpg");
+//		gsm = new GameStateManager();
+//		gsm.push(new MainMenu(gsm));
+//		gsm.render(batch);
+
 
 	@Override
 	public void render () {
+		// System.out.println("VIrus war "+ gsm.peek());
 		gsm.render(batch);
-//		ScreenUtils.clear(1, 0, 0, 1);
+
+//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//		stage.act(Gdx.graphics.getDeltaTime());
+//		stage.draw();
+		//	ScreenUtils.clear(1, 0, 0, 1);
 //		batch.begin();
 //		batch.draw(img, 0, 0);
 //		batch.end();
-//		menu.render(batch);
 	}
-	
+
 	@Override
 	public void dispose () {
-		batch.dispose();
-		//img.dispose();
+//		batch.dispose();
+//		img.dispose();
 	}
 }

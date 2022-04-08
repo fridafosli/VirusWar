ffb9d77b1ee71f8ce3678462f4134ecf4bc8992a
Therese <therese.sigmundstad@gmail.com>
2022-04-08 19:48:02 +0200

    Add back and customize button to GameLobby

--- core/src/no/ntnu/viruswar/States/GameLobby.java
+++ core/src/no/ntnu/viruswar/States/GameLobby.java
@@ -3,7 +3,10 @@ package no.ntnu.viruswar.States;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class GameLobby extends StateMenu {
 
@@ -15,8 +18,28 @@ public class GameLobby extends StateMenu {
         this.host = host;
         this.pin = pin;
 
-        Label l = new Label("Game Pin: " + this.pin, skin);
-        table.add(l);
+        Label pinLabel = new Label("Game Pin: " + this.pin, skin);
+        table.add(pinLabel);
+        TextButton backBtn = new TextButton("Back", skin);
+        TextButton customizeBtn = new TextButton("Customize", skin);
+        backBtn.setPosition(0, Gdx.graphics.getHeight() - 70);
+        backBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.log("Back", "clicked");
+                gsm.pop();
+            }
+        });
+        customizeBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.log("Custom", "clicked");
+                gsm.push(new Custom(gsm));
+            }
+        });
+        customizeBtn.setPosition((Gdx.graphics.getWidth()/2)-40, 10);
+        stage.addActor(backBtn);
+        stage.addActor(customizeBtn);
 
 
 

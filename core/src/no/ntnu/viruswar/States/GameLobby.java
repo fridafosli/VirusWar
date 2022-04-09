268061c292b2ce026915c662afbb2dba2d588a4c
Frida Fosli <frida.fosli@gmail.com>
2022-04-09 16:01:36 +0200

    Added backbutton and playbutton

--- core/src/no/ntnu/viruswar/States/GameLobby.java
+++ core/src/no/ntnu/viruswar/States/GameLobby.java
@@ -3,22 +3,61 @@ package no.ntnu.viruswar.States;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class GameLobby extends StateMenu {
 
     private boolean host;
     private String pin;
+    private TextButton backBtn;
+    private TextButton playBtn;
 
     protected GameLobby(final GameStateManager gsm, boolean host, String pin) {
         super(gsm);
         this.host = host;
         this.pin = pin;
 
+        // Set up Back-button
+        backBtn = new TextButton("Back", skin);
+        backBtn.setPosition(0, Gdx.graphics.getHeight() - 70);
+        backBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.log("back", "clicked");
+                gsm.pop();
+            }
+        });
+        stage.addActor(backBtn);
+        table.row();
+
         Label l = new Label("Game Pin: " + this.pin, skin);
+        l.setPosition(100, 20);
         table.add(l);
+        table.row();
+
+        Label p = new Label("Players: ", skin);
+        p.setPosition(100, 50);
+        table.add(p);
+        table.row();
 
 
+        // Set up Play-button
+        playBtn = new TextButton("Play", skin);
+        playBtn.setPosition(Gdx.graphics.getWidth() - 200, 50);
+        playBtn.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.log("play", "clicked");
+                gsm.pop();
+            }
+        });
+        stage.addActor(playBtn);
 
     }
 

1c242a0eb614cf52fea15eb5e6216292639cda4c
Therese <therese.sigmundstad@gmail.com>
2022-04-12 14:10:18 +0200

    Try to update player to custom character

--- core/src/no/ntnu/viruswar/Data/Player.java
+++ core/src/no/ntnu/viruswar/Data/Player.java
@@ -1,5 +1,8 @@
 package no.ntnu.viruswar.Data;
 
+import com.badlogic.gdx.graphics.Color;
+
+import java.util.Random;
 import java.util.UUID;
 
 public class Player {
@@ -10,6 +13,7 @@ public class Player {
     private String skin;
     private String name;
     private String id;
+    private String color;
 
     public Player() {
         // Default constructor required for calls to DataSnapshot.getValue(Player.class)
@@ -21,9 +25,20 @@ public class Player {
         this.points = points;
         this.skin = skin;
         this.name = name;
+        this.color= generateColor();
         id = UUID.randomUUID().toString();
     }
 
+    public String generateColor(){
+        Random rand = new Random();
+        int myRandomNumber = rand.nextInt(0x10) + 0x10;
+        System.out.printf("%x\n",myRandomNumber);
+        return Integer.toHexString(myRandomNumber);
+    }
+
+    public void setColor(String color){
+        this.color=color;
+    }
     public float getX() {
         return x;
     }
@@ -35,6 +50,10 @@ public class Player {
     public void setPosition(float x, float y) {
         this.x = x;
         this.y = y;
+    }
+
+    public String getColor(){
+        return this.color;
     }
 
     public float getPoints() {
@@ -62,6 +81,7 @@ public class Player {
         return "Player{" +
                 "name='" + name + '\'' +
                 ", id='" + id + '\'' +
+                ", color='" + color + '\'' +
                 '}';
     }
 }

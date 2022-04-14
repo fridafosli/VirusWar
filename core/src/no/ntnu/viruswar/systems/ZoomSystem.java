6454608f6dabd51be3e5de0087a075a76005e796
epdoan <epdoan@stud.ntnu.no>
2022-04-14 16:46:06 +0200

    add zooming

---
+++ core/src/no/ntnu/viruswar/systems/ZoomSystem.java
@@ -0,0 +1,59 @@
+package no.ntnu.viruswar.systems;
+
+import com.badlogic.ashley.core.ComponentMapper;
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+
+import no.ntnu.viruswar.Camera;
+import no.ntnu.viruswar.componenets.ConsumableComponent;
+import no.ntnu.viruswar.componenets.DimensionComponent;
+import no.ntnu.viruswar.componenets.HiddenComponent;
+import no.ntnu.viruswar.componenets.PlayerComponent;
+import no.ntnu.viruswar.componenets.TransformComponent;
+
+public class ZoomSystem extends IteratingSystem {
+    private final ComponentMapper<ConsumableComponent> consumableMapper;
+    private final ComponentMapper<TransformComponent> transformMapper;
+    private final ComponentMapper<DimensionComponent> rectangleMapper;
+
+
+    private final Array<Entity> entityQueue;
+    private Camera camera;
+
+    private float prevSize = 0;
+
+    public ZoomSystem(Camera camera) {
+        super(Family.all(ConsumableComponent.class, TransformComponent.class, DimensionComponent.class).exclude(HiddenComponent.class).get());
+        this.consumableMapper = ComponentMapper.getFor(ConsumableComponent.class);
+        this.transformMapper = ComponentMapper.getFor(TransformComponent.class);
+        this.rectangleMapper = ComponentMapper.getFor(DimensionComponent.class);
+        this.entityQueue = new Array<>();
+        this.camera = camera;
+
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        super.update(deltaTime);
+        Entity clientPlayer = getEngine().getEntitiesFor(Family.all(PlayerComponent.class).get()).get(0);
+        Vector3 clientPos = transformMapper.get(clientPlayer).position;
+        if (prevSize == 0) {
+            prevSize =  consumableMapper.get(clientPlayer).size;
+        }
+        if(prevSize < consumableMapper.get(clientPlayer).size) {
+            prevSize =  consumableMapper.get(clientPlayer).size;
+            camera.zoom();
+        }
+        entityQueue.clear();
+
+    }
+
+
+    @Override
+    protected void processEntity(Entity entity, float deltaTime) {
+        entityQueue.add(entity);
+    }
+}

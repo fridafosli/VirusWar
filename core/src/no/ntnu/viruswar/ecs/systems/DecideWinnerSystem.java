1f457f23cff72cd62772d2643c8d60a5000be777
Victoria Hopso <victoria.hopso@gmail.com>
2022-04-17 20:11:40 +0200

    add system for deciding winner

---
+++ core/src/no/ntnu/viruswar/ecs/systems/DecideWinnerSystem.java
@@ -0,0 +1,40 @@
+package no.ntnu.viruswar.ecs.systems;
+
+import com.badlogic.ashley.core.Entity;
+import com.badlogic.ashley.core.Family;
+import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.utils.Array;
+
+import no.ntnu.viruswar.context.Context;
+import no.ntnu.viruswar.ecs.componenets.PlayerComponent;
+
+public class DecideWinnerSystem extends IteratingSystem {
+
+    private final Array<Entity> entityQueue;
+    private final Context context;
+
+    public DecideWinnerSystem(Context context) {
+        super(Family.all(PlayerComponent.class).get());
+        entityQueue = new Array<>();
+        this.context = context;
+    }
+
+    @Override
+    public void update(float deltaTime) {
+        super.update(deltaTime);
+        if (entityQueue.size == 1) {
+            //the player is the winner
+            //context.getScreens().push(SETT INN END SCREEN);
+            //set winner in db?
+            //remove player from game in db?
+            
+
+
+        }
+    }
+
+    @Override
+    protected void processEntity(Entity entity, float deltaTime) {
+        entityQueue.add(entity);
+    }
+}

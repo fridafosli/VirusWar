12f0c2dcdf9b354195b2eaca7b7aee09866cdbd7
Therese <therese.sigmundstad@gmail.com>
2022-04-21 13:10:07 +0200

    Fix settings

--- core/src/no/ntnu/viruswar/screens/Settings.java
+++ core/src/no/ntnu/viruswar/screens/Settings.java
@@ -5,7 +5,10 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Container;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Slider;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
@@ -13,22 +16,51 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import no.ntnu.viruswar.context.Context;
 
 public class Settings extends ContextScreen {
-
     protected Stage stage;
     protected Skin skin;
     protected Sprite background;
-    private final TextButton backBtn;
 
     public Settings(final Context context) {
         super(context);
         // Create the skin
         skin = new Skin(Gdx.files.internal("uiskin.json"));
-        //skin.getFont("default-font").getData().setScale(scale);
+        skin.getFont("default-font").getData().setScale(Gdx.graphics.getHeight() / 300);
 
+        //skin.getFont("default-font").getData().setScale(scale);
         stage = new Stage(new ScreenViewport());
 
+        Label volumeLabel= new Label("Set music volume",skin);
+        volumeLabel.setPosition((Gdx.graphics.getWidth()-400)/2,750);
+        stage.addActor(volumeLabel);
+        Label highVolume= new Label("100%",skin);
+        highVolume.setPosition(Gdx.graphics.getWidth()-600,560);
+        stage.addActor(highVolume);
+        Label lowVolume= new Label("0%",skin);
+        lowVolume.setPosition(Gdx.graphics.getWidth()-1500,560);
+        stage.addActor(lowVolume);
+        // Sets volume slider
+        final Slider slider= new Slider( 0,  1,  0.1f, false, skin);
+        slider.setVisualPercent(context.getAssets().getVolume());
+        slider.addListener(new ClickListener() {
+
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.log("slider", "interacted");
+                context.getAssets().setMusic(true, slider.getPercent());
+
+            }
+        });
+        Container<Slider> container=new Container<>(slider);
+        container.setTransform(true);
+
+        container.setPosition((Gdx.graphics.getWidth()-container.getWidth())/2,600);
+        container.setScale(Gdx.graphics.getHeight() / 250);
+
+        stage.addActor(container);
+
+        // stage.addActor(slider);
         // Setting up the back button
-        backBtn = new TextButton("Back", skin);
+        TextButton backBtn = new TextButton("Back", skin);
         backBtn.setPosition(0, Gdx.graphics.getHeight() - 50);
         backBtn.addListener(new ClickListener() {
 
@@ -36,13 +68,13 @@ public class Settings extends ContextScreen {
             public void clicked(InputEvent event, float x, float y) {
                 Gdx.app.log("Settings", "clicked");
                 context.getScreens().pop();
+
             }
         });
-        stage.addActor(backBtn);
 
+        stage.addActor(backBtn);
         // Set the background
-        background = new Sprite(new Texture(Gdx.files.internal("basicBackground.png")));
-        background.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
     }
 
 
@@ -55,7 +87,6 @@ public class Settings extends ContextScreen {
     public void render(float dt) {
         //Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         context.getBatch().begin();
-        background.draw(context.getBatch());
         context.getBatch().end();
 
         stage.act(dt);

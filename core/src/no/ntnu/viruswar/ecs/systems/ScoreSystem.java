3d84e5d2115a6bac9d7d0b8c2d053ea064a7e6ae
Victoria Hopso <victoria.hopso@gmail.com>
2022-04-24 00:50:13 +0200

    refactor

--- core/src/no/ntnu/viruswar/ecs/systems/ScoreSystem.java
+++ core/src/no/ntnu/viruswar/ecs/systems/ScoreSystem.java
@@ -1,39 +1,26 @@
 package no.ntnu.viruswar.ecs.systems;
 
-import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
-import com.badlogic.gdx.utils.Array;
-import no.ntnu.viruswar.context.Context;
-import no.ntnu.viruswar.ecs.componenets.DimensionComponent;
 import no.ntnu.viruswar.ecs.componenets.HiddenComponent;
-import no.ntnu.viruswar.ecs.componenets.IdentifierComponent;
 import no.ntnu.viruswar.ecs.componenets.LeadTextComponent;
 import no.ntnu.viruswar.ecs.componenets.PlayerComponent;
-import no.ntnu.viruswar.ecs.componenets.TransformComponent;
-import no.ntnu.viruswar.services.backend.BackendModel;
 import no.ntnu.viruswar.services.models.Player;
 import no.ntnu.viruswar.services.lobby.LobbyController;
-import no.ntnu.viruswar.utils.Constants;
 
 public class ScoreSystem extends IteratingSystem {
 
-    private final Array<Entity> entityQueue;
-    private final ComponentMapper<IdentifierComponent> idMapper;
-
     private final LobbyController lobbyController;
     private LeadTextComponent textComp = new LeadTextComponent();
-    private float leadpoints = 0;
-    private String lead = "tie";
+    private float leadPoints = 0;
+    private String lead;
     private final Engine engine;
 
     public ScoreSystem(Engine engine, LobbyController lobbycontroller) {
         super(Family.all(PlayerComponent.class).exclude(HiddenComponent.class).get());
         this.lobbyController = lobbycontroller;
-        this.idMapper = ComponentMapper.getFor(IdentifierComponent.class);
-        entityQueue = new Array<Entity>();
         this.engine = engine;
         Entity ent = engine.createEntity();
         ent.add(textComp);
@@ -43,33 +30,27 @@ public class ScoreSystem extends IteratingSystem {
     @Override
     public void update(float deltaTime) {
         super.update(deltaTime);
-        this.leadpoints = 0;
+        this.leadPoints = 0;
 
-        for (Player player: lobbyController.getPlayers().values()){
-            if (player.getPoints() > leadpoints) {
-                leadpoints = player.getPoints();
-                lead = player.getName();
-            }
-        }
-        /*
-        for (Entity entity : entityQueue) {
-            IdentifierComponent idc = idMapper.get(entity);
-            Player p = lobbyController.getPlayers().get(idc.id);
-            if (p.getPoints() > leadpoints) {
-                leadpoints = p.getPoints();
+        for (Player p: lobbyController.getPlayers().values()){
+            if (p.getPoints() > leadPoints) {
+                leadPoints = p.getPoints();
                 lead = p.getName();
             }
-        }*/
+        }
+        if (leadPoints == 50){ //NB CHANGE TO 0 I POINTS CHANGED
+            textComp.leadPlayer = "Tie";
 
-        textComp.leadPlayer = "Lead: " + lead + " " + leadpoints + " points";
-
-        entityQueue.clear();
+        }
+        else {
+            textComp.leadPlayer = "Lead: " + lead + " " + leadPoints + " points";
+        }
     }
 
 
     @Override
     protected void processEntity(Entity entity, float deltaTime) {
-        entityQueue.add(entity);
+
     }
 }
 
